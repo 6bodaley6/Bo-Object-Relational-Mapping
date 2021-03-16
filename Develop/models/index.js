1bd4a537b108b759971ba9711042b2b7bd31afd1
Bo Daley <70559889+6bodaley6@users.noreply.github.com>
2021-03-15 21:15:09 -0600

    started on tag routes still need to complete

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -4,14 +4,32 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+//??cardinality 1-1 || 1-many || many to many these are the 3 different cardinalities 
 // Products belongsTo Category
+Product.belongsTo(Category, { foreignKey: "category_id" });
 
 // Categories have many Products
+// // Category.hasMany(Product);
+/* we define on both sides of the relationship becuase that is how sequalize is
+ * abile to set up the functions that you would call that you would retrieve to 
+ * set and get data from the relationship 
+ */
 
 // Products belongToMany Tags (through ProductTag)
+// // Product.belongsToMany(Tag, { through: ProductTag, foreignKey: "product_id" });
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, { through: ProductTag, foreignKey: "tag_id" })
+Product.belongsToMany(Tag, { through: ProductTag, foreignKey: "product_id" })
 
+// // ProductTag.belongsTo(Product, { foreignKey: "product_id" });
+// // ProductTag.belongsTo(Tag, { foreignKey: "tag_id" });
+// // Product.hasMany(ProductTag, { foreignKey: "product_id" });
+Category.hasMany(Product, { foreignKey: "category_id" })
+// // Tag.hasMany(ProductTag, { foreignKey: "tag_id" })
+/*I am creating this many to many relationship between product and tag 
+ * the new table will belong to both product and tag using the foreign keys
+ */
 module.exports = {
   Product,
   Category,
--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -2,26 +2,50 @@ const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
-
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    include: [
+      {
+        model: Product,
+        through: ProductTag,
+      },]
+  })
+    .then(dbTagData => res.json(dbTagData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get('/:id', (req, res) => {
+  //!! Tag.findone()
   // find a single tag by its `id`
   // be sure to include its associated Product data
 });
 
 router.post('/', (req, res) => {
-  // create a new tag
+  Tag.create(req.body).then((dbTagData) => {
+    res.json(dbTagData)
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 router.put('/:id', (req, res) => {
+  Tag.update(req.body, { where: { id: req.params.id } }).then((dbTagData) => {
+    res.json(dbTagData)
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
   // update a tag's name by its `id` value
 });
 
 router.delete('/:id', (req, res) => {
+  //!! Tag.destroy()
   // delete on tag by its `id` value
 });
 
