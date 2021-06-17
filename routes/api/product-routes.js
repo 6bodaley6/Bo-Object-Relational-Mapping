f6235a49938d451d8e7c95b2d7b7c3240c9118e0
Bo Daley <70559889+6bodaley6@users.noreply.github.com>
2021-06-16 18:31:12 -0600

    added async to api/product-routes need to do the same for tag-routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -19,9 +19,25 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Category and Tag data
 });
 
-
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [
+        Category,
+        { model: Tag, through: ProductTag }]
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'This specific product was not found' });
+      return;
+    }
+
+    res.status(200).json(productData);
+
+  } catch (error) {
+    res.status(500).json(err)
+  }
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
 });
@@ -100,8 +116,20 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: { id: req.params.id }
+    });
+    if (!productData) {
+      res.status(400).json({ message: 'Specific product not found' });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (error) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
